b227228f86c91b00aca8d32760a9966a7d343dee
Mikkel Dengsoee <mikkeld@google.com>
2016-02-07 15:25:07 +0000

    Initial Commit

---
+++ DataWizz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DataWizz
+//
+//  Created by Mikkel Dengsøe on 2/7/16.
+//  Copyright © 2016 Mikkel Dengsøe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
