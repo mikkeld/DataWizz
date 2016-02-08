0857991964bf8b304d1c0ad736b67fcf47b38e46
Mikkel Dengsoee <mikkeld@google.com>
2016-02-08 10:15:08 +0000

    Pre table view controller

---
+++ DataWizz/Dataset.swift
@@ -0,0 +1,19 @@
+//
+//  Dataset.swift
+//  DataWizz
+//
+//  Created by Mikkel Dengsøe on 2/7/16.
+//  Copyright © 2016 Mikkel Dengsøe. All rights reserved.
+//
+
+import Foundation
+
+class Stories {
+    
+    var headline = ""
+    var subHeadline = ""
+    var chart = ""
+    var summary = ""
+    var story = ""
+    
+}
--- DataWizz/ViewController.swift
+++ DataWizz/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  DataWizz
-//
-//  Created by Mikkel Dengsøe on 2/7/16.
-//  Copyright © 2016 Mikkel Dengsøe. All rights reserved.
-//
 
 import UIKit
 
@@ -12,14 +5,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("test")
+        
+        let stockMarket = Stories()
+        stockMarket.headline = "Boom and burst"
+        stockMarket.subHeadline = "The rise and fall of the S&P 500"
+        stockMarket.summary = "Through the last ten year blah blah"
+        stockMarket.story = "Here comes a long story"
+        stockMarket.chart = "stockmarket.jpg"
+        
+        headlineLabel.text = stockMarket.headline
+        subheaderLabel.text = stockMarket.subHeadline
+        chart.image = UIImage(named: stockMarket.chart)
+        summaryLabel.text = stockMarket.summary
+        bodyLabel.text = stockMarket.story
+        
+        let url = NSURL(string: "https://api.github.com/users/mralexgray/repos")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if let urlContent = data {
+                
+                do {
+                    
+                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
+                    
+                } catch {
+                    
+                    print("JSON failed")
+                    
+                }
+                
+                
+                
+            }
+        }
+        
+        task.resume()
+        
     }
+    
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var subheaderLabel: UILabel!
+    @IBOutlet weak var chart: UIImageView!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
+    
+
+    
 
 }
 
